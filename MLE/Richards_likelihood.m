2c526ba92b488f3ce9d75720368010cad5b4f73f
Matthew Simpson <39819988+ProfMJSimpson@users.noreply.github.com>
2021-06-01 15:29:21 +1000

    Add files via upload

---
+++ MLE/Richards_likelihood.m
@@ -0,0 +1,5 @@
+function [e] = Richards_likelihood(beta, lambda, K, N0, sigma, t,Ndata)
+N = K*N0./(N0^beta+(K^beta-N0^beta).*exp(-1*beta*lambda*t')).^(1/beta);
+e=0;
+y= log(normpdf(N',Ndata,sigma)); 
+e=sum(y);
